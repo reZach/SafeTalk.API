27d71044704cd2fe765203aa7fc22ebf520bcdd7
Zachary <zwenta@carthage.edu>
2017-03-10 23:05:09 -0600

    added comment where testing will happen

--- SafeTalk.Tests/UnitTest1.cs
+++ SafeTalk.Tests/UnitTest1.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SafeTalk.Tests
 {
+    // https://docs.microsoft.com/en-us/aspnet/web-api/overview/testing-and-debugging/unit-testing-with-aspnet-web-api
     [TestClass]
     public class UnitTest1
     {
