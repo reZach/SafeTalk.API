15c4cfe3ca5f1886e84195e4957c63d307500813
Zachary <zwenta@carthage.edu>
2017-03-11 18:24:14 -0600

    added config files so the web.config file can be mocked

--- SafeTalk.Tests/TestUserController.cs
+++ SafeTalk.Tests/TestUserController.cs
@@ -1,24 +1,29 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using SafeTalk.API.Controllers;
 using SafeTalk.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace SafeTalk.Tests
 {
     [TestClass]
+    [DeploymentItem("sharedAppSettings.config")]
+    [DeploymentItem("secretConnectionStrings.config")]
     public class TestUserController
     {
-        [TestMethod]
+        [TestMethod]        
         public void Post_ShouldReturnNewUser()
         {
             var controller = new UserController();
 
-            var result = controller.Post() as User;
-            Assert.AreNotEqual(result, null);
+            var fakeUser = new User();
+            IHttpActionResult response = controller.Post();
+            var contentResult = response as OkNegotiatedContentResult<User>;
+
+            Assert.AreEqual(contentResult.Content.GetType(), fakeUser.GetType());
         }
 
 
